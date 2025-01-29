4db73410dac1fb9f0e609d9a2392754ded6580c3
Giorgos Mastoris <giorgos.mastoris@spitogatos.gr>
2025-01-29 22:48:35 +0200

    RedirectIfAuthenticated Middleware

--- routes/web.php
+++ routes/web.php
@@ -3,15 +3,21 @@
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\MovieController;
+use App\Http\Middleware\RedirectIfAuthenticated;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/login', function () {
-    return Inertia::render('Auth/Login');
-})->name('login');
+
 
 Route::get('/', [MovieController::class, 'index'])->name('index');
-Route::get('/register', [RegisterController::class, 'create'])->name('register');
-Route::post('/register', [RegisterController::class, 'store'])->name('register.store');
-Route::post('/login', [LoginController::class, 'store'])->name('login.store');
+
+Route::middleware([RedirectIfAuthenticated::class])->group(function () {
+    Route::get('/login', function () {
+        return Inertia::render('Auth/Login');
+    })->name('login');
+    Route::get('/register', [RegisterController::class, 'create'])->name('register');
+    Route::post('/register', [RegisterController::class, 'store'])->name('register.store');
+    Route::post('/login', [LoginController::class, 'store'])->name('login.store');
+});
 Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
+
